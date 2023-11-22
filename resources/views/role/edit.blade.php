ca142b33d0a44fca5a9b93b9ab24e0936dcca88f
Mohd Fitri Hazeq Bin Abu Hassan <phitriharxeq@gmail.com>
2023-11-22 10:59:22 +0800

    adding selection in edit role

--- resources/views/role/edit.blade.php
+++ resources/views/role/edit.blade.php
@@ -12,7 +12,21 @@
         <div class="row">
             <div class="col mb-3">
                 <label class="form-label">Role</label>
-                <input type="text" name="role_name" class="form-control" placeholder="Role" value="{{ $role->role_name }}" >
+                <!-- <input type="text" name="role_name" class="form-control" placeholder="Role" value="{{ $role->role_name }}" > -->
+                <select class="form-control" name="role_name" value="{{ $role->role_name }}">
+                    <option value="MSA" @if($role->role_name == 'MSA') selected @endif>Master Super Admin</option>
+                    <option value="AC" @if($role->role_name == 'AC') selected @endif>Account</option>
+                    <option value="Admin" @if($role->role_name == 'Admin') selected @endif>Administrator</option>
+                    <option value="CM" @if($role->role_name == 'CM') selected @endif>Contract Manager</option>
+                    <option value="PD" @if($role->role_name == 'PD') selected @endif>Profile Director</option>
+                    <option value="PM" @if($role->role_name == 'PM') selected @endif>Profile Manager</option>
+                    <option value="PURC" @if($role->role_name == 'PURC') selected @endif>Purchaser</option>
+                    <option value="QS" @if($role->role_name == 'QS') selected @endif>Quality Surveyor</option>
+                    <option value="SA" @if($role->role_name == 'SA') selected @endif>Super Admin</option>
+                    <option value="Site" @if($role->role_name == 'Site') selected @endif>Site</option>
+                    <option value="SSA" @if($role->role_name == 'SSA') selected @endif>Super Super Admin</option>
+                    <option value="VIEW" @if($role->role_name == 'VIEW') selected @endif>Viewing</option>
+                </select>
             </div>
         </div>
         <div class="form-group">
