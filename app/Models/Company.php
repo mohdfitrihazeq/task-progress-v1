807f9f85d6c7665010192455d945289de268597b
luciusfong <6cillefong@gmail.com>
2023-11-27 10:54:28 +0800

    Update Company.php

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -1,10 +1,7 @@
 <?php
-
 namespace App\Models;
-
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
 class Company extends Model
 {
     use HasFactory;
@@ -12,12 +9,8 @@ class Company extends Model
     protected $fillable = [
         'company_name'
     ];
-    protected $primaryKey = 'company_id';
-
-
     public function projects()
     {
         return $this->belongsToMany(Project::class, 'project_company', 'company_id', 'project_id')->withTimestamps();
     }
-
 }
