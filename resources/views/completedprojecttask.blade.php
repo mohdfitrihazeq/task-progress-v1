0bdbeb70e7dfa474012f661f9029f5b7be03aabc
luciusfong <6cillefong@gmail.com>
2023-12-03 04:04:39 +0800

    completed task export

--- resources/views/completedprojecttask.blade.php
+++ resources/views/completedprojecttask.blade.php
@@ -22,6 +22,11 @@
             {{ Session::get('success') }}
         </div>
     @endif
+    @if($projecttaskprogress->count() > 0)
+        @foreach($projecttaskprogress as $rs)
+            <input hidden name="exportrow" data-project="{{ $rs->project_id }}" value="{{ '<tr><td>'.$rs->task_sequence_no_wbs.'</td></tr>' }}">
+        @endforeach
+    @endif
     <form>
         <table class="table table-hover" id="data-table">
             <thead class="table-primary">
@@ -31,7 +36,8 @@
                     <th>Actual Start Date</th>
                     <th>Actual End Date</th>
                     <th>Task Progress %</th>
-                    <th>Project</th>
+                    <th hidden>Project</th>
+                    <th hidden>Project ID</th>
                 </tr>
             </thead>
             <tbody>
@@ -43,7 +49,8 @@
                             <td class="align-middle">{{ $rs->task_actual_start_date }}</td>
                             <td class="align-middle">{{ $rs->task_actual_end_date }}</td>
                             <td class="align-middle">{{ $rs->task_progress_percentage }}</td>
-                            <td class="align-middle">{{ $rs->project_name }}</td>
+                            <td class="align-middle" hidden>{{ $rs->project_name }}</td>
+                            <td class="align-middle" hidden>{{ $rs->project_id }}</td>
                         </tr>
                     @endforeach
                 @else
@@ -53,8 +60,12 @@
                 @endif
             </tbody>
         </table>
+        <input hidden id="includeheader" value="disabled">
+        <input hidden id="includeproject" value="disabled">
+        <input hidden id="dateformat" value="uk">
     </form>
-
+    <button id="btnExport" onclick="fnExcelReport();"> EXPORT </button>
+    <iframe id="txtArea1" style="display:none"></iframe>
     <script src="{{asset('admin_assets/js/completedprojecttask.js')}}"></script>
     <div class="d-flex align-items-center justify-content-between bg-primary p-2 text-white">
         <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-file" viewBox="0 0 16 16" id="usadateformat" name="usadateformat" onclick="usaDateFormat() "><path d="M4 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H4zm0 1h8a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1z"/></svg>
@@ -65,6 +76,74 @@
         Project column included in Excel
     </div>
 <script>    
+function fnExcelReport() {
+    var tab_text = "<table border='2px'><tr>";
+    var j = 0;
+    var tab = document.getElementById('data-table'); // id of table
+    var projectFilter = document.getElementById('projectFilter').value; 
+    var includeHeader = document.getElementById('includeheader').value;
+    var includeProject = document.getElementById('includeproject').value;
+    var dateFormat = document.getElementById('dateformat').value;
+
+    if(includeHeader=="enabled"){
+        var header = tab.rows[0].getElementsByTagName("th");
+        for (l = 0; l < 5; l++) {
+            tab_text = tab_text + header[l].outerHTML;
+        }
+        if(includeProject=="enabled"){
+            tab_text = tab_text + header[5].outerHTML;
+        }
+        tab_text = tab_text + "</tr><tr>";
+    }
+    for (j = 1; j < tab.rows.length; j++) {
+        var data = tab.rows[j].getElementsByTagName("td");
+        for (k = 0; k < 2; k++) {
+            if(data[6].innerHTML==projectFilter||projectFilter==""){
+                tab_text = tab_text + data[k].outerHTML;
+            }
+        }
+        for (k = 2; k < 4; k++) {
+            if(data[6].innerHTML==projectFilter||projectFilter==""){
+                if(dateFormat=="uk"){
+                    tab_text = tab_text + "<td style='mso-number-format:" + "yyyy-mm-dd" + "'>" + data[k].innerHTML + "</td>";
+                }
+                else{
+                    tab_text = tab_text + "<td style='mso-number-format:" + "mm/dd/yyyy" + "'>" + data[k].innerHTML + "</td>";
+                }
+            }
+        }
+        if(data[6].innerHTML==projectFilter||projectFilter==""){
+            tab_text = tab_text + data[4].outerHTML;
+        }
+        if((data[6].innerHTML==projectFilter||projectFilter=="")&&includeProject=="enabled"){
+            tab_text = tab_text + data[5].outerHTML;
+        }
+        tab_text = tab_text + "</tr>";
+        //tab_text=tab_text+"</tr>";
+    }
+
+    tab_text = tab_text + "</table>";
+    tab_text = tab_text.replace(/<A[^>]*>|<\/A>/g, "");//remove if u want links in your table
+    tab_text = tab_text.replace(/<img[^>]*>/gi, ""); // remove if u want images in your table
+    tab_text = tab_text.replace(/<input[^>]*>|<\/input>/gi, ""); // reomves input params
+
+    var msie = window.navigator.userAgent.indexOf("MSIE ");
+
+    // If Internet Explorer
+    if (msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./)) {
+        txtArea1.document.open("txt/html", "replace");
+        txtArea1.document.write(tab_text);
+        txtArea1.document.close();
+        txtArea1.focus();
+
+        sa = txtArea1.document.execCommand("SaveAs", true, "Say Thanks to Sumit.xls");
+    } else {
+        // other browser not tested on IE 11
+        sa = window.open('data:application/vnd.ms-excel,' + encodeURIComponent(tab_text));
+    }
+
+    return sa;
+}
     $(document).ready(function () {
         var table = document.getElementById('data-table');
         var rows = table.getElementsByTagName('tr');
@@ -87,99 +166,6 @@
                     rows[i].style.display='table-row';
                 }
             }
-        });
-        var table = $('#data-table').DataTable({
-            dom: 'Bfrtip', // Add the export buttons to the DOM
-            buttons: [
-                {
-                    extend: 'excelHtml5',
-                    text: 'wo header',
-                    header: false,
-                    exportOptions: {
-                        columns: ':visible',
-                    }
-                },
-                {
-                    extend: 'excelHtml5',
-                    text: 'w header',
-                    exportOptions: {
-                        columns: ':visible',
-                        rows: ":not('.data-table-header')",
-                    }
-                },
-                {
-                    text: 'USA Date Format',
-                    action: function ( e, dt, node, config ) {
-                        table.rows().every(function(){
-                            if(this.data()[2][4]=="-"){
-                                let date = new Date(this.data()[2]);
-                                let day = date.getDate().toString().padStart(2,"0");
-                                let month = date.getMonth()+1;
-                                let year = date.getFullYear();
-                                this.data()[2]=month+"/"+day+"/"+year;
-                                date = new Date(this.data()[3]);
-                                day = date.getDate().toString().padStart(2,"0");
-                                month = date.getMonth()+1;
-                                year = date.getFullYear();
-                                this.data()[2]=month+"/"+day+"/"+year;
-                            }
-                            else{
-                                let date = new Date(this.data()[2]);
-                                let day = date.getDate().toString().padStart(2,"0");
-                                let month = date.getMonth()+1;
-                                let year = date.getFullYear();
-                                this.data()[2]=year+"-"+month+"-"+day;
-                                date = new Date(this.data()[3]);
-                                day = date.getDate().toString().padStart(2,"0");
-                                month = date.getMonth()+1;
-                                year = date.getFullYear();
-                                this.data()[3]=year+"-"+month+"-"+day;
-                            }
-                        });
-                        var htmltable = document.getElementById('data-table');
-                        var htmlrows = htmltable.getElementsByTagName('tr');
-
-                        // Loop through each <tr> element and log its content
-                        for (var i = 1; i < htmlrows.length; i++) {
-                            if(htmlrows[i].getElementsByTagName("td")[2].innerHTML[4]=="-"){
-                                let htmldate = new Date(htmlrows[i].getElementsByTagName("td")[2].innerHTML);
-                                let htmlday = htmldate.getDate().toString().padStart(2,"0");
-                                let htmlmonth = htmldate.getMonth()+1;
-                                let htmlyear = htmldate.getFullYear();
-                                htmlrows[i].getElementsByTagName("td")[2].innerHTML=htmlmonth+"/"+htmlday+"/"+htmlyear;
-                                htmldate = new Date(htmlrows[i].getElementsByTagName("td")[2].innerHTML);
-                                htmlday = htmldate.getDate().toString().padStart(2,"0");
-                                htmlmonth = htmldate.getMonth()+1;
-                                htmlyear = htmldate.getFullYear();
-                                htmlrows[i].getElementsByTagName("td")[3].innerHTML=htmlmonth+"/"+htmlday+"/"+htmlyear;
-                            }
-                            else{
-                                let htmldate = new Date(htmlrows[i].getElementsByTagName("td")[2].innerHTML);
-                                let htmlday = htmldate.getDate().toString().padStart(2,"0");
-                                let htmlmonth = htmldate.getMonth()+1;
-                                let htmlyear = htmldate.getFullYear();
-                                htmlrows[i].getElementsByTagName("td")[2].innerHTML=htmlyear+"-"+htmlmonth+"-"+htmlday;
-                                htmldate = new Date(htmlrows[i].getElementsByTagName("td")[2].innerHTML);
-                                htmlday = htmldate.getDate().toString().padStart(2,"0");
-                                htmlmonth = htmldate.getMonth()+1;
-                                htmlyear = htmldate.getFullYear();
-                                htmlrows[i].getElementsByTagName("td")[3].innerHTML=htmlyear+"-"+htmlmonth+"-"+htmlday;
-                            }
-                        }
-                    }
-                },
-                {
-                    text: 'Project column included in Excel',
-                    action: function ( e, dt, node, config ) {
-                        if(table.columns( [5] ).visible()[0]==true){
-                            table.columns( [5] ).visible( false );
-                        }
-                        else{
-                            table.columns( [5] ).visible( true );
-                        }
-                    }
-                },
-            ],
         });
     });
 </script>
